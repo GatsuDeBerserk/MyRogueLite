00f3ec3624ec45001a2991bf469c0440ace9bdff
LT <jotaro.ludt@gmail.com>
2026-02-18 20:24:50 -0500

    added some bullshit, experimentation UI

--- myRogueLite/src/main/java/org/Controler.java
+++ myRogueLite/src/main/java/org/Controler.java
@@ -1,11 +1,19 @@
 package org;
 
 import javafx.scene.*;
-import org.game.locations.Kingdom;
+import org.game.locations.Kingdom;import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.lang.System;
 
 public class Controler {
-    private
     static void main() {
+        Scene scene=new Scene(new Group());
+        scene.getStylesheets().add("org/ui/style.css");
+
+
         Kingdom allo = new Kingdom();
         allo.getKingdom();
     }
