809d2aad54aa4994924f7f6f1cc1cf7aac8f0b03
LT <jotaro.ludt@gmail.com>
2026-02-19 16:21:03 -0500

    progression avec javaFX

--- myRogueLite/src/main/java/org/Controler.java
+++ myRogueLite/src/main/java/org/Controler.java
@@ -1,6 +1,9 @@
 package org;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
+import javafx.scene.layout.Pane;
 import org.game.locations.Kingdom;import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
@@ -8,13 +11,25 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.lang.System;
 
-public class Controler {
-    static void main() {
-        Scene scene=new Scene(new Group());
-        scene.getStylesheets().add("org/ui/style.css");
+public class Controler extends Application {
+    public static void main(String[] args) {
+        launch(args);
+
 
 
         Kingdom allo = new Kingdom();
         allo.getKingdom();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("titre");
+
+//        Scene scene=new Scene(new Group());
+//        scene.getStylesheets().add("org/ui/style.css");
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ui/jeux.fxml"));
+        Pane pane =new Pane();
+        primaryStage.show();
+
     }
 }
