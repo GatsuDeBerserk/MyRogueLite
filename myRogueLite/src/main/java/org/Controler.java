77d5669b7981cd20b76b918d7e5c6c000d769b19
LT <jotaro.ludt@gmail.com>
2026-02-18 20:05:50 -0500

    added some bullshit, experimentation UI

---
+++ myRogueLite/src/main/java/org/Controler.java
@@ -0,0 +1,12 @@
+package org;
+
+import javafx.scene.*;
+import org.game.locations.Kingdom;
+
+public class Controler {
+    private
+    static void main() {
+        Kingdom allo = new Kingdom();
+        allo.getKingdom();
+    }
+}
