61422f2e2fce0611cbae36c6ddd74ab62fc05007
LT <jotaro.ludt@gmail.com>
2026-02-22 18:43:38 -0500

    taponnageFX

--- myRogueLite/src/main/java/org/Controler.java
+++ myRogueLite/src/main/java/org/Controler.java
@@ -3,18 +3,23 @@ package org;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.Pane;
-import org.game.locations.Kingdom;import javafx.beans.value.ObservableValue;
+import org.game.locations.Kingdom;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.ui.GaphicController;
+
 import java.lang.System;
 
 public class Controler extends Application {
     public static void main(String[] args) {
         launch(args);
-
 
 
         Kingdom allo = new Kingdom();
@@ -23,12 +28,24 @@ public class Controler extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        GaphicController gc = new GaphicController();
         primaryStage.setTitle("titre");
 
-//        Scene scene=new Scene(new Group());
-//        scene.getStylesheets().add("org/ui/style.css");
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ui/jeux.fxml"));
-        Pane pane =new Pane();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("org/ui/jeux.fxml"));
+
+        TabPane pane = new TabPane();
+//        TitledPane tab =new TitledPane();
+        Tab tab = new Tab();
+        tab.setText("allo");
+        fxmlLoader.setController(gc);
+        tab.setContent(fxmlLoader.load());
+        pane.getTabs().add(tab);
+
+        Scene scene = new Scene(pane);
+        scene.getStylesheets().add("org/ui/style.css");
+
+
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
